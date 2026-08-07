471e2df8815b800a7a5843d4b7009f8ed383df24
Bristi Ghosh <116305028+BristiGhosh604@users.noreply.github.com>
2026-08-07 13:55:03 +0530

    Add config file and config loader

---
+++ src/config.py
@@ -0,0 +1,9 @@
+import yaml
+
+def load_config(path="config.yaml"):
+    with open(path, "r") as f:
+        return yaml.safe_load(f)
+
+if __name__ == "__main__":
+    config = load_config()
+    print(config)
